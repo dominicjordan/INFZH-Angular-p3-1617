d7074170a11b1a3b297a7eee3fb2921ebba1c04d
Dominic Jahangier <dominicjahangier@MacBook-Pro-van-Dominic.local>
2017-02-22 12:38:14 +0100

    working version of the app

--- AngularJS ToDo/app.js
+++ AngularJS ToDo/app.js
@@ -5,17 +5,30 @@ module.controller('todoController', function($scope) {
 
 	$scope.categories = ["Groceries", "Work", "School", "Lunch"];
 
-	$scope.items = [
-		{name:"Milk", checked:false, category:"Groceries"},
-		{name:"Eggs", checked:false, category:"Groceries"},
-		{name:"Cheese", checked:true, category:"Groceries"},
-		{name:"Do Homework", checked:false, category:"School"},
-		{name:"Buy pencils", checked:true, category:"School"},
-		{name:"Copy code from codebin", checked:false, category:"School"},
-		{name:"Make an Angular test", checked:false, category:"Work"},
-		{name:"Push code to Github", checked:false, category:"Work"},
-		{name:"Yet Another Item", checked:true, category:"Work"}
-	];
+
+	var localItems = JSON.parse(localStorage.getItem("items"));
+
+	if(localItems != undefined && localItems.length>0) {
+		$scope.items = localItems;
+	}
+	else {
+		$scope.items = [
+			{name:"Milk", checked:false, category:"Groceries"},
+			{name:"Eggs", checked:false, category:"Groceries"},
+			{name:"Cheese", checked:true, category:"Groceries"},
+			{name:"Do Homework", checked:false, category:"School"},
+			{name:"Buy pencils", checked:true, category:"School"},
+			{name:"Copy code from codebin", checked:false, category:"School"},
+			{name:"Make an Angular test", checked:false, category:"Work"},
+			{name:"Push code to Github", checked:false, category:"Work"},
+			{name:"Yet Another Item", checked:true, category:"Work"}
+		];
+	}
+
+
+
+
+	
 
 	$scope.deleteItem = function(item) {
 
@@ -27,6 +40,8 @@ module.controller('todoController', function($scope) {
 		});
 
 		$scope.items.splice(deleteIndex, 1);
+
+		localStorage.setItem("items", JSON.stringify($scope.items));
 	};
 
 	$scope.saveNewItem = function() {
@@ -39,6 +54,14 @@ module.controller('todoController', function($scope) {
 		});
 
 		$scope.newItem = {};
+
+		localStorage.setItem("items", JSON.stringify($scope.items));
+	};
+
+	$scope.updateItem = function(item) {
+		item.updating=false;
+
+		localStorage.setItem("items", JSON.stringify($scope.items));
 	};
 
 
